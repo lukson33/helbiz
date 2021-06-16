9409c747e1baf9b134c9f6387c71342628437e88
lukson33 <45272632+lukson33@users.noreply.github.com>
2021-06-16 21:39:14 +0200

    tab functionality changes and added animation

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const company = document.querySelector(".nav-company");
 const product = document.querySelector(".nav-product");
 const about = document.querySelector(".nav-about");
 const aboutBtn = document.querySelector(".about-btn");
+const tabBtns = [...document.querySelectorAll(".tab-btn")];
 
 //navigation
 hamburger.addEventListener("click", () => {
@@ -17,30 +18,40 @@ hamburger.addEventListener("click", () => {
   }
 });
 
-//product tab in nav functionality
-product.addEventListener("click", () => {
-  //add and remove classes from active tabs
-  company.classList.remove("nav-active");
-  product.classList.add("nav-active");
+//tab buttons in nav functionality
+tabBtns.map((btn) => {
+  btn.addEventListener("click", () => {
+    //get tab buttons
+    const buttonArr = [...document.querySelector(".nav-btns").children];
+    //find the index of clicked tab button in the array
+    const btnIndex = buttonArr.findIndex(
+      (item) => item.classList === btn.classList
+    );
+    //find the tab according to index of clicked button
+    const choosenTab = document.querySelector(".nav-tabs").children[btnIndex];
+    //go through all of the tabs
+    [...document.querySelector(".nav-tabs").children].map((tab) => {
+      //check if tab is the one that is not chosen
+      if (tab !== choosenTab) {
+        //fade out the tab that wasn't chosen
+        tab.classList.add("fade-out");
+        tab.classList.remove("fade-in");
+      }
+    });
+    //fade in the tab that was choosen
+    setTimeout(() => {
+      choosenTab.classList.remove("fade-out");
+      choosenTab.classList.add("fade-in");
+    }, 300);
 
-  document.querySelector(".nav-list-5").classList.remove("hidden");
-  document.querySelector(".nav-list-5").classList.add("block");
-
-  document.querySelector(".nav-list-9").classList.add("hidden");
-  document.querySelector(".nav-list-9").classList.remove("block");
-});
-
-//company tab in nav functionality
-company.addEventListener("click", () => {
-  //add and remove classes from active tabs
-  product.classList.remove("nav-active");
-  company.classList.add("nav-active");
-
-  document.querySelector(".nav-list-9").classList.remove("hidden");
-  document.querySelector(".nav-list-9").classList.add("block");
-
-  document.querySelector(".nav-list-5").classList.add("hidden");
-  document.querySelector(".nav-list-5").classList.remove("block");
+    //blue border animation
+    //calculate how much and where border should move - 105px * the index of the button that was clicked
+    const borderPos = 105 * btnIndex;
+    //move border to calculated position
+    document.querySelector(
+      ".blue-border"
+    ).style.transform = `translate(${borderPos}px)`;
+  });
 });
 
 //about button functionality
